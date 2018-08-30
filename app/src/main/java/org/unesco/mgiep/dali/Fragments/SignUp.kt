b305b0630a12c10d3e5ed2737c3dc14bcf7407a8
DonAnk <11donank22@gmail.com>
2018-08-30 18:07:15 +0530

    firestore repository | fetch data methods

--- app/src/main/java/org/unesco/mgiep/dali/Fragments/SignUp.kt
+++ app/src/main/java/org/unesco/mgiep/dali/Fragments/SignUp.kt
@@ -11,18 +11,23 @@ import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_registration.*
 import org.unesco.mgiep.dali.Data.Gender
+import org.unesco.mgiep.dali.Data.User
 import org.unesco.mgiep.dali.R
+import org.unesco.mgiep.dali.Repositories.FirebaseRepository
 import org.unesco.mgiep.dali.Utility.showFragment
-
+import java.util.*
 
 
 class SignUp :Fragment() {
 
     private var gender = Gender.MALE
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var firebaseReposirtory: FirebaseRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mAuth = FirebaseAuth.getInstance()
+        firebaseReposirtory = FirebaseRepository()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?=
@@ -40,7 +45,7 @@ class SignUp :Fragment() {
                 edit_register_confirm_password.text != edit_register_password.text -> edit_register_confirm_password.error = getString(R.string.confirm_password_diff_error)
                 edit_register_school.text.isEmpty() -> edit_register_school.error = getString(R.string.required)
                 edit_register_age.text.isEmpty() -> edit_register_age.error = getString(R.string.required)
-                edit_register_age.text.toString().toInt() == 0 -> edit_register_age.error = getString(R.string.invalid_age)
+                edit_register_age.text.toString().toInt() <= 0 -> edit_register_age.error = getString(R.string.invalid_age)
                 edit_register_designation.text.isEmpty() -> edit_register_designation.error = getString(R.string.required)
                 !edit_radio_male.isChecked && !edit_radio_female.isChecked -> {
                     Toast.makeText(activity, getString(R.string.select_gender),Toast.LENGTH_SHORT).show()
@@ -57,6 +62,17 @@ class SignUp :Fragment() {
                     mAuth.createUserWithEmailAndPassword(edit_register_email.text.toString(), edit_register_password.text.toString())
                             .addOnCompleteListener { task ->
                                 if (task.isSuccessful) {
+                                    firebaseReposirtory.writeNewUser(
+                                            mAuth.currentUser!!.uid,
+                                            User(
+                                                    mAuth.currentUser!!.email.toString(),
+                                                    edit_register_name.text.toString(),
+                                                    edit_register_designation.text.toString(),
+                                                    edit_register_school.text.toString(),
+                                                    edit_register_age.text.toString().toInt(),
+                                                    gender.toString()
+                                            )
+                                    )
                                     showFragment(
                                             Fragment.instantiate(
                                                     activity,
@@ -71,6 +87,7 @@ class SignUp :Fragment() {
                                             Toast.LENGTH_SHORT).show()
                                 }
                             }
+
                 }
             }
         }
--- app/src/main/java/org/unesco/mgiep/dali/Repositories/FirebaseRepository.kt
+++ app/src/main/java/org/unesco/mgiep/dali/Repositories/FirebaseRepository.kt
@@ -1,23 +1,25 @@
 package org.unesco.mgiep.dali.Repositories
 
-import android.database.Observable
 import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 import org.unesco.mgiep.dali.Data.*
-import java.util.*
 
 
 class FirebaseRepository {
-    private val mDatabase = FirebaseFirestore.getInstance();
+    private val mDatabase = FirebaseFirestore.getInstance()
+    private val usersRef = mDatabase.collection("users")
+    private val screeningsRef = mDatabase.collection("screenings")
+    private val participantsRef = mDatabase.collection("participants")
+    private val resultsRef = mDatabase.collection("results")
 
     fun writeNewUser(userId: String, user: User) {
 
-        mDatabase.collection("user").document(userId).set(user)
+        usersRef.document(userId).set(user)
     }
 
-    fun writeNewScreening(id:String, screening: Screening ){
+    fun writeNewScreening(id:String, screening: FirebaseScreening ){
 
-        mDatabase.collection("screening").document(id).set(screening)
+        screeningsRef.document(id).set(screening)
                 .addOnSuccessListener {
                     Log.d("save-screening","success")
                 }
@@ -27,11 +29,162 @@ class FirebaseRepository {
     }
 
     fun writeNewParticipant(id: String, participant: Participant){
-        mDatabase.collection("participant").document(id).set(participant)
+        participantsRef.document(id).set(participant)
     }
 
     fun writeResult(id: String, result: Result){
-        mDatabase.collection("result").document(id).set(result)
+        resultsRef.document(id).set(result)
+    }
+
+    fun fetchUserDetails(userId: String){
+        usersRef.document(userId).get().addOnCompleteListener {
+            task ->
+            if(task.isSuccessful){
+                if(task.result.exists()){
+                    val user = task.result.toObject(User::class.java)
+                    Log.d("fetch-userDetails","success")
+                }else{
+                    Log.d("fetch-userDetails","failed!!No Such Document")
+                }
+            }else{
+                Log.d("fetch-userDetails","failed",task.exception)
+            }
+        }
+    }
+
+    fun fetchScreening(screeningId: String){
+        screeningsRef.document(screeningId).get().addOnCompleteListener {
+            task ->
+            if(task.isSuccessful){
+                if(task.result.exists()){
+                    val screening = task.result.toObject(FirebaseScreening::class.java)
+                    Log.d("fetch-screening","success")
+                }else{
+                    Log.d("fetch-screening","failed!!No Such Document")
+                }
+            }else{
+                Log.d("fetch-screening","failed",task.exception)
+            }
+        }
+    }
+
+    fun fetchUserScreenings(userId: String){
+        screeningsRef
+                .whereEqualTo("userId",userId)
+                .get()
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                         if(!task.result.isEmpty){
+                                Log.d("fetch-screenings","success")
+                         }else{
+                                Log.d("fetch-screenings","failed!!No Such Document")
+                         }
+                    }else{
+                        Log.d("fetch-screening","failed",task.exception)
+                    }
+                }
+    }
+
+    fun fetchParticipantScreenings(participantId: String){
+        screeningsRef
+                .whereEqualTo("participantId",participantId)
+                .get()
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        if(!task.result.isEmpty){
+                            Log.d("fetch-PScreenings","success")
+                        }else{
+                            Log.d("fetch-PScreenings","failed!!No Such Document")
+                        }
+                    }else{
+                        Log.d("fetch-screening","failed",task.exception)
+                    }
+                }
+    }
+
+    fun fetchParticipant(participantId: String){
+        participantsRef.document(participantId)
+                .get()
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        if(task.result.exists()){
+                            val participant = task.result.toObject(Participant::class.java)
+                            Log.d("fetch-Participant","success")
+                        }else{
+                            Log.d("fetch-Participant","failed!!No Such Document")
+                        }
+                    }else{
+                        Log.d("fetch-Participant","failed",task.exception)
+                    }
+                }
+    }
+
+    fun fetchParticipants(userId: String){
+        participantsRef
+                .whereEqualTo("userId",userId)
+                .get()
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        if(!task.result.isEmpty){
+                            Log.d("fetch-Participants","success")
+                        }else{
+                            Log.d("fetch-Participants","failed!!No Such Document")
+                        }
+                    }else{
+                        Log.d("fetch-Participants","failed",task.exception)
+                    }
+                }
+    }
+
+    fun fetchResult(resultId: String){
+        resultsRef
+                .document(resultId)
+                .get()
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        if(!task.result.exists()){
+                            Log.d("fetch-Result","success")
+                        }else{
+                            Log.d("fetch-Result","failed!!No Such Document")
+                        }
+                    }else{
+                        Log.d("fetch-Result","failed",task.exception)
+                    }
+                }
+    }
+
+    fun fetchParticipantResults(participantId: String){
+        resultsRef
+                .whereEqualTo("participantId",participantId)
+                .get()
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        if(!task.result.isEmpty){
+                            Log.d("fetch-PResults","success")
+                        }else{
+                            Log.d("fetch-PResults","failed!!No Such Document")
+                        }
+                    }else{
+                        Log.d("fetch-PResults","failed",task.exception)
+                    }
+                }
+    }
+
+    fun fetchScreeningResult(screeningId: String){
+        resultsRef
+                .whereEqualTo("screeningId",screeningId)
+                .get()
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        if(!task.result.isEmpty){
+                            Log.d("fetch-SResults","success")
+                        }else{
+                            Log.d("fetch-SResults","failed!!No Such Document")
+                        }
+                    }else{
+                        Log.d("fetch-SResults","failed",task.exception)
+                    }
+                }
     }
 
 }

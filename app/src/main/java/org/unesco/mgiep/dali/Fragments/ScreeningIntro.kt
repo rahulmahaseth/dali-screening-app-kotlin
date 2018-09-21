8608bad35798ea1d857e7c18662fa476a3ca39a8
DonAnk <11donank22@gmail.com>
2018-09-21 11:02:24 +0530

    Guide Fragments + dimension files

---
+++ app/src/main/java/org/unesco/mgiep/dali/Fragments/ScreeningIntro.kt
@@ -0,0 +1,22 @@
+package org.unesco.mgiep.dali.Fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.unesco.mgiep.dali.R
+
+class ScreeningIntro : Fragment(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.fragment_screeningintro, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
